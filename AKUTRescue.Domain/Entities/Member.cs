2d7a778ffbd7eb06f289daabb172174b018aad3e
Murat GÖKTAŞ <mcs.goktas@gmail.com>
2025-03-05 18:08:55 +0300

    08- Member class changed. It added ctor

--- AKUTRescue.Domain/Entities/Member.cs
+++ AKUTRescue.Domain/Entities/Member.cs
@@ -16,6 +16,20 @@ public class Member : Entity<Guid>
     public Guid AuthorityId { get; set; }
     public Guid TeamId { get; set; }
     public string ProfilePhotoUrl { get; set; } = "#";
+    public Member()
+    {
+        
+    }
+    public Member(Guid id, string firstName,string? middleName, string lastName, string email )
+    {
+        Id = id;
+        FirstName = firstName;
+        if (middleName != null) { MiddleName = middleName; }
+        LastName = lastName;
+        FullName = $"{FirstName} {MiddleName} {LastName}";
+        Email = email;
+    }
+
     // Navigation properties
     public virtual Authority Authority { get; set; }
     public virtual Team Team { get; set; }
